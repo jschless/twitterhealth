e57127326c7a6081b175be21ec2d3ed580785cb7
jschless <joe.c.schlessinger@gmail.com>
2019-01-29 06:28:28 -0800

    commenting

--- classifier.py
+++ classifier.py
@@ -1,6 +1,3 @@
-'''
-trains a neural network dataset according to engineered features
-'''
 import pandas as pd
 import phemeParser
 from sklearn.neural_network import MLPClassifier
@@ -14,12 +11,18 @@ def crossValidation(X, y):
     print('todo')
 
 def kfold(mod, X, y, n_splits=5):
+    """Trains model using kfold cross validation
+
+    Keyword arguments:
+    mod -- the model to be trained
+    X -- input matrix
+    y -- output matrix
+    n_splits -- optional number of splits to apply to dataset
+    """
+
     totalScore = 0
     kf = KFold(n_splits=n_splits)
-    best = None
-    bestMod = None
-    worst = None
-    confusionMat = None
+    best, bestMod, worst, confusionMat = None, None, None, None
     for train_index, test_index in kf.split(X):
         newMod = mod.fit(X.iloc[train_index], y.iloc[train_index])
         score = mod.score(X.iloc[test_index], y.iloc[test_index])
@@ -40,6 +43,12 @@ def kfold(mod, X, y, n_splits=5):
     return bestMod
 
 def initializeDataset(data):
+    """Takes raw list of threads and creates pandas dataframe. Important because it removes unannotated data
+
+    Keyword arguments:
+    data -- list of threads
+    """
+
     inputs = pd.DataFrame.from_records([thread.to_dict() for thread in data if len(thread.annotation) > 0]).set_index('tweetid')
     labels = pd.DataFrame.from_dict([thread.annotation[0] for thread in data if len(thread.annotation) > 0], orient = 'columns').set_index('tweetid')
 
@@ -47,19 +56,33 @@ def initializeDataset(data):
     return combined
 
 def buildInputAndLabels(data, label='certainty'):
+    """Outputs input matrix and label matrix
+
+    Keyword arguments:
+    data -- pandas dataframe
+    """
+
     data = data.dropna(subset=[label])
     X = data[['favorite_count', 'retweet_count']]
     y = data[label].apply(lambda x: classificationMap[x])
     return X, y
 
 def buildInput(data):
+    """Outputs input vectors for unlabeled datasets
+
+    Keyword arguments:
+    data -- pandas dataframe
+    """
+
     return data[['favorite_count', 'retweet_count']]
 
 def run(listOfThreads, testTweets):
-    '''
+    """Trains model and makes predictions for unlabeled set of tweets
+
+
     listOfThreads: input from PHEME to train datasets
     testTweets: list of Tweets to predict on
-    '''
+    """
     data = initializeDataset(listOfThreads)
     X, y = buildInputAndLabels(data)
     clf = MLPClassifier(solver='lbfgs')
@@ -68,6 +91,7 @@ def run(listOfThreads, testTweets):
         model.predict(listToDF(testTweets))
 
 def listToDF(inList):
+    """Turns a list of tweets to a pandas dataframe"""
     return buildInput(pd.DataFrame.from_records([tweet.to_dict() for tweet in inList]))
 
 def main(testTweets=None):
