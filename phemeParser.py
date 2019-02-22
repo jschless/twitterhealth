d6da904028599f743e46f09c06dfa21942f76ba7
Joe Schless <joe.c.schlessinger@gmail.com>
2019-02-22 16:17:14 -0500

    fixed phemeParser style

--- phemeParser.py
+++ phemeParser.py
@@ -6,14 +6,20 @@ from itertools import chain
 from tweet import *
 from anytree import Node, RenderTree, DoubleStyle, AsciiStyle
 
-### USER VARIABLES ###
-#Link to PHEME dataset: https://figshare.com/articles/PHEME_rumour_scheme_dataset_journalism_use_case/2068650 #
 
-### Replace with location of PHEME dataset. Should be something like C:\...Documents\PHEME ###
+# Link to PHEME dataset:
+# https://figshare.com/articles/PHEME_rumour_scheme_dataset_journalism_use_case/2068650
+
+# Replace with location of PHEME dataset. C:\...Documents\PHEME
 pathToPheme = 'C:\\Users\\EECS\\Documents'
-annotationFile = pathToPheme + '\\PHEME\\pheme-rumour-scheme-dataset\\annotations\\en-scheme-annotations.json'
+annotationFile = pathToPheme + '\\PHEME\\pheme-rumour-scheme-dataset
+\\annotations\\en-scheme-annotations.json'
+
 with open(annotationFile) as f:
-    annotations = pd.DataFrame([json.loads(line) for line in f if not '#' in line])
+    annotations = pd.DataFrame(
+        [json.loads(line) for line in f if '#' not in line]
+    )
+
 
 def loadAnnotations(path):
     """Returns annotation dataframe for all tweets in PHEME dataset
@@ -22,9 +28,13 @@ def loadAnnotations(path):
     path -- path to the location of the PHEME dataset
     """
 
-    annotationFile = path + '\\PHEME\\pheme-rumour-scheme-dataset\\annotations\\en-scheme-annotations.json'
+    annotationFile = path + '\\PHEME\\pheme-rumour-scheme-dataset
+    \\annotations\\en-scheme-annotations.json'
     with open(annotationFile) as f:
-        return pd.DataFrame([json.loads(line) for line in f if not '#' in line])
+        return pd.DataFrame(
+            [json.loads(line) for line in f if '#' not in line]
+        )
+
 
 def crawlDirectory(path):
     """crawls PHEME directory and returns list of all conversation threads
@@ -33,11 +43,14 @@ def crawlDirectory(path):
     path -- path to directory
     """
     path += '\\PHEME\\pheme-rumour-scheme-dataset\\threads\\en'
-    allThreads= []
+    # TODO: replace with flatten
+    allThreads = []
 
-    for threads in [processCategory(path + '\\' + dirName) for dirName in os.listdir(path)]:
+    for threads in
+    [processCategory(path + '\\' + dirName) for dirName in os.listdir(path)]:
         allThreads += threads
     return allThreads
+
 
 def processCategory(path):
     """Processes a PHEME tweet topic
@@ -45,7 +58,9 @@ def processCategory(path):
     Keyword arguments:
     path -- path to the topics
     """
-    return [processTweetFolder(path + '\\' + tweetFolder, tweetFolder) for tweetFolder in os.listdir(path)]
+    return [processTweetFolder(path + '\\' + tweetFolder, tweetFolder)
+            for tweetFolder in os.listdir(path)]
+
 
 def processTweetFolder(path, tweetid):
     """Processes an entire thread and returns a tweet with all replies
@@ -54,18 +69,20 @@ def processTweetFolder(path, tweetid):
     path -- path to threads
     tweetid -- tweetid of thread
     """
-    thread = processTweetJSON(path+'\\source-tweets\\' + tweetid + '.json', False)
-    with open(path +  '\\annotation.json') as f:
+    thread = processTweetJSON(path + '\\source-tweets\\' +
+                              tweetid + '.json', False)
+    with open(path + '\\annotation.json') as f:
         thread.thread_annotation = json.load(f)
         thread.thread_annotation['tweetid'] = tweetid
-    with open(path +  '\\structure.json') as f:
+    with open(path + '\\structure.json') as f:
         thread.thread_structure = json.load(f)
     root = Node(str(tweetid), tweet=thread)
     processTree(thread.thread_structure[tweetid], root, path + '\\reactions\\')
     thread.reply_chain = root
-    #print(RenderTree(root, style=AsciiStyle))
+    # print(RenderTree(root, style=AsciiStyle))
     thread.thread_id = tweetid
     return thread
+
 
 def processTree(children, parent, path):
     """Returns tree of replies
@@ -77,8 +94,10 @@ def processTree(children, parent, path):
     if not children:
         return
     for key, value in children.items():
-        temp = Node(str(key), parent=parent, tweet=processTweetJSON(path + key + '.json', True))
+        temp = Node(str(key), parent=parent,
+                    tweet=processTweetJSON(path + key + '.json', True))
         processTree(value, temp, path)
+
 
 def processTweetJSON(path, is_reply):
     """Processes and returns an individual tweet JSON
@@ -93,10 +112,19 @@ def processTweetJSON(path, is_reply):
         userData = data['user']
         user = User()
         tweet = Tweet()
-        user.phemeUser(userData['name'], userData['screen_name'], userData['favourites_count'], userData['followers_count'], userData['description'], userData['verified'], userData['friends_count'])
-        tweet.phemeTweet(data['text'], data['favorite_count'], data['retweet_count'], data['id_str'], is_reply, user)
-        tweet.annotation = annotations[annotations['tweetid'] == tweet.tweetid].to_dict('r') #keeps dataframe id out of the mix
+        user.phemeUser(userData['name'], userData['screen_name'],
+                       userData['favourites_count'],
+                       userData['followers_count'],
+                       userData['description'],
+                       userData['verified'], userData['friends_count'])
+        tweet.phemeTweet(data['text'], data['favorite_count'],
+                         data['retweet_count'], data['id_str'], is_reply, user)
+        tweet.annotation = annotations[
+            annotations['tweetid'] == tweet.tweetid
+        ].to_dict('r')
+        # keeps dataframe id out of the mix
         return tweet
+
 
 def parsePheme(pathToPheme):
     """Parses PHEME dataset and returns a list of all conversation threads
