2fa76425ff75097f2b73ccf9e0ed9dbdead19af4
jschless <joe.c.schlessinger@gmail.com>
2019-02-26 08:20:31 -0800

    restyle

--- classifier.py
+++ classifier.py
@@ -8,10 +8,11 @@ from features import *
 import matplotlib.pyplot as plt
 pathToPheme = 'C:\\Users\\EECS\\Documents'
 
+
 class Classifier:
     def __init__(self):
         self.threads = pd.DataFrame.from_dict(
-            [thread.to_dict() for thread in phemeParser.parsePheme(pathToPheme)]
+            [t.to_dict() for t in phemeParser.parsePheme(pathToPheme)]
         )
         self.model = MLPClassifier(solver='lbfgs')
         self.run()
@@ -39,16 +40,22 @@ class Classifier:
                 best = score
                 bestMod = newMod
                 predictionY = bestMod.predict(X.iloc[test_index])
-                confusionMat = ConfusionMatrix(y.iloc[test_index].values, predictionY)
+                confusionMat = ConfusionMatrix(
+                    y.iloc[test_index].values, predictionY
+                )
             if worst is None or score < worst:
                 worst = score
             totalScore += score
 
-        print('average score for %s tests is %s' % (n_splits, totalScore/n_splits))
+        print(
+            'average score for %s tests is %s' % (
+                n_splits, totalScore/n_splits
+            )
+        )
         print('best score was %s\n worst score was %s' % (best, worst))
-        #confusionMat.plot()
-        #confusionMat.print_stats()
-        #plt.show()
+        # confusionMat.plot()
+        # confusionMat.print_stats()
+        # plt.show()
         self.model = bestMod
 
     def buildInputAndLabels(self, data, label='misinformation'):
@@ -61,7 +68,6 @@ class Classifier:
         y = data['thread_annotation'].apply(convert_annotations)
         return X, y
 
-
     def buildInput(self, data):
         """Outputs input vectors for unlabeled datasets
 
@@ -69,16 +75,14 @@ class Classifier:
         data -- input list of threads
         """
 
-        """This is where the features from features.py are integrated"""
+        # This is where the features from features.py are integrated
         inputs = pd.DataFrame()
         inputs['follow_ratio'] = data['user'].apply(follow_ratio)
-        #inputs['sentiment'] = data['text'].apply(sentiment)
+        # inputs['sentiment'] = data['text'].apply(sentiment)
         return inputs
-
 
     def run(self, testTweets=None):
         """Trains model and makes predictions for unlabeled set of tweets
-
 
         listOfThreads: input from PHEME to train datasets
         testTweets: list of Tweets to predict on
@@ -90,7 +94,6 @@ class Classifier:
         if testTweets is not None:
             testX = self.buildInput(testTweets)
             self.model.predict(textX)
-
 
     def predict(self, tweet):
         df = pd.DataFrame.from_dict([tweet.to_dict()])
