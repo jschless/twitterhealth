3d7e234674f5829d04d274f2f81e72ed7dd09fc2
jschless <joe.c.schlessinger@gmail.com>
2019-03-05 08:01:39 -0800

    fixed dated warning

--- classifier.py
+++ classifier.py
@@ -6,6 +6,8 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import KFold
 from pandas_ml import ConfusionMatrix
 from features import *
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
 import matplotlib.pyplot as plt
 
 class Classifier:
@@ -27,17 +29,15 @@ class Classifier:
 
         totalScore = 0
         kf = KFold(n_splits=n_splits)
-        best, bestMod, worst, confusionMat = None, None, None, None
+        best, bestMod, worst, y_pred, y_true = None, None, None, None, None
         for train_index, test_index in kf.split(X):
             newMod = self.model.fit(X.iloc[train_index], y.iloc[train_index])
             score = self.model.score(X.iloc[test_index], y.iloc[test_index])
             if best is None or score > best:
                 best = score
                 bestMod = newMod
-                predictionY = bestMod.predict(X.iloc[test_index])
-                confusionMat = ConfusionMatrix(
-                    y.iloc[test_index].values, predictionY
-                )
+                y_pred = bestMod.predict(X.iloc[test_index])
+                y_true = y.iloc[test_index].values
             if worst is None or score < worst:
                 worst = score
             totalScore += score
@@ -47,10 +47,10 @@ class Classifier:
                 n_splits, totalScore/n_splits
             )
         )
-        print('best score was %s\n worst score was %s' % (best, worst))
+        print('best score was %s\nworst score was %s' % (best, worst))
         if verbose:
-            confusionMat.plot()
-            confusionMat.print_stats()
+            class_names = np.array(['false', 'true', 'unverified'])
+            plot_confusion_matrix(y_true, y_pred, classes=class_names)
             plt.show()
         self.model = bestMod
 
@@ -103,3 +103,57 @@ class Classifier:
         index = probMap[prediction[0]]
         prob = probMat[0, index]/np.sum(probMat[0])
         return prediction, prob
+
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    print(unique_labels(y_true, y_pred))
+    # classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
