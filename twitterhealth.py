d8c5e2b6cb84fcc37970034627c2882b6e7e49f3
jschless <joe.c.schlessinger@gmail.com>
2019-03-05 06:53:37 -0800

    twitterhealth style

--- twitterhealth.py
+++ twitterhealth.py
@@ -1,14 +1,21 @@
-import sys, getopt, classifier, twitterGUI
+import sys
+import getopt
+import classifier
+import twitterGUI
+
+
 def main(argv):
     help = 'main.py [-c -t -v]\n'
     help += '-c or classifier : only runs classifier\n'
     help += '-t or twitter : runs classifier within twitter GUI\n'
     help += '-v or verbose : prints extra information'
     try:
-        opts, args = getopt.getopt(argv, "htcv", ['twitter', 'classifier', 'verbose'])
+        opts, args = getopt.getopt(
+            argv, "htcv", ['twitter', 'classifier', 'verbose']
+        )
     except getopt.GetoptError:
-      print(help)
-      sys.exit(2)
+        print(help)
+        sys.exit(2)
     if opts == []:
         print(help)
         sys.exit(0)
@@ -20,7 +27,7 @@ def main(argv):
             print(help)
             sys.exit()
         elif opt in ('-t', '--twitter'):
-            twitter=True
+            twitter = True
         elif opt in ('-c', '--classifier'):
             clas = True
         elif opt in ('-v', '--verbose'):
@@ -34,5 +41,7 @@ def main(argv):
             clf.run(verb=True)
         else:
             clf.run()
+
+
 if __name__ == '__main__':
     main(sys.argv[1:])
